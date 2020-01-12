ae358e88135def81d50b99be27e6654537e796ac
Kaz Yoshikawa <kaz.yoshikawa@gmail.com>
2020-01-12 09:16:30 -0500

    added ZFramedButton

---
+++ Sources/ZFramedButton.swift
@@ -0,0 +1,21 @@
+//
+//	ZFramedButton.swift
+//	ZKit
+//
+//	Created by Kaz Yoshikawa on 10/16/19.
+//	Copyright © 2019 Electricwoods LLC. All rights reserved.
+//
+
+import UIKit
+
+
+public class ZFramedButton: UIButton {
+
+	override public func layoutSubviews() {
+		super.layoutSubviews()
+		self.setBorder(color: self.titleColor(for: self.state), width: 1.0)
+		self.layer.cornerRadius = 3.0
+	}
+
+}
+
