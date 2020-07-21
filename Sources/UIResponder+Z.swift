18cb962b1c45290269f0e75a0d230aed594b49d4
Kaz Yoshikawa <kaz.yoshikawa@gmail.com>
2020-07-21 06:16:35 -0400

    added UIResponder extension

---
+++ Sources/UIResponder+Z.swift
@@ -0,0 +1,37 @@
+//
+//	UIResponder+Z.swift
+//	ZKit
+//
+//	Created by Kaz Yoshikawa on 4/14/16.
+//
+//
+
+import Foundation
+import UIKit
+
+
+extension UIResponder {
+	
+	func findViewController<T: UIViewController>() -> T? {
+		var responder = self.next
+		repeat {
+			if let viewController = responder as? T {
+				return viewController
+			}
+			responder = responder!.next
+		} while responder != nil
+		return nil
+	}
+	
+	func findResponder<T>() -> T? {
+		var responder = self.next
+		repeat {
+			if let viewController = responder as? T {
+				return viewController
+			}
+			responder = responder!.next
+		} while responder != nil
+		return nil
+	}
+
+}
