8985f2d23b5bd5f21c97a52e54302a8e300bbcc6
Kaz Yoshikawa <kaz@digitallynx.com>
2024-02-19 21:33:34 -0500

    refactoring hexadecimalString() and init?(hexadecimalString:)

--- Sources/ZKit/Data+Z.swift
+++ Sources/ZKit/Data+Z.swift
@@ -10,13 +10,16 @@ import Foundation
 
 public extension Data {
 
-	func hexadecimalString() -> String {
-		let n = 16
-		var string = ""
-		for base in stride(from: 0, to: self.count, by: n) {
-			string += ((0 ..< n).compactMap { base+$0 < self.count ? String(format: "%02x", self[base+$0]) : nil }.joined()) + "\r"
-		}
-		return string
+	func hexadecimalString(linebreak: Int? = nil) -> String {
+		let n = linebreak ?? self.count
+		return stride(from: 0, to: self.count, by: n).map { base in (0..<n).map { String(format: "%02x", self[base + $0]) }.joined() }.joined(separator: "\r")
+	}
+	init?(hexadecimalString: String) {
+		guard hexadecimalString.filter({ !$0.isWhitespace && !$0.isHexDigit }).count > 0 else { return nil }
+		let hexdigits = hexadecimalString.filter { $0.isHexDigit }.map { $0.hexDigitValue! }
+		guard hexdigits.count > 0 || hexdigits.count % 2 == 0 else { return nil }
+		let bytes = stride(from: 0, to: hexdigits.count, by: 2).map { UInt8(Int(hexdigits[$0] * 0x01 + hexdigits[$0 + 1])) }
+		self = Data(bytes)
 	}
 
 }
