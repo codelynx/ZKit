0f5c301ff496b0c595ce197da3eaab2c4a9dea9d
Kaz Yoshikawa <kaz@digitallynx.com>
2024-06-03 13:09:56 -0400

    hexadecimal ignores newlines

--- Sources/ZKit/Data+Z.swift
+++ Sources/ZKit/Data+Z.swift
@@ -15,7 +15,7 @@ public extension Data {
 		return stride(from: 0, to: self.count, by: n).map { base in (0..<n).map { String(format: "%02x", self[base + $0]) }.joined() }.joined(separator: "\r")
 	}
 	init?(hexadecimalString: String) {
-		guard hexadecimalString.filter({ !$0.isWhitespace && !$0.isHexDigit }).count > 0 else { return nil }
+		guard hexadecimalString.filter({ !$0.isWhitespace && !$0.isNewline && !$0.isHexDigit }).count > 0 else { return nil }
 		let hexdigits = hexadecimalString.filter { $0.isHexDigit }.map { $0.hexDigitValue! }
 		guard hexdigits.count > 0 || hexdigits.count % 2 == 0 else { return nil }
 		let bytes = stride(from: 0, to: hexdigits.count, by: 2).map { UInt8(Int(hexdigits[$0] * 0x01 + hexdigits[$0 + 1])) }
